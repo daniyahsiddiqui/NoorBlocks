ee3268d89e1b9f3df2c1a9dd52df14becd48890f
Daniyah Siddiqui <daniyah@gmail.com>
2026-05-31 14:27:46 -0500

    config: update supabase credentials

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@
 // Fill in with your Supabase project credentials.
 // If left blank, NoorBlocks automatically operates in Offline (LocalStorage) Mode.
 const SUPABASE_CONFIG = {
-  url: "",
-  anonKey: ""
+  url: "https://regesfrafmetvxlhnhjp.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlZ2VzZnJhZm1ldHZ4bGhuaGpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyNDA3MjMsImV4cCI6MjA5NTgxNjcyM30.E2AyV12tUyPSajn7lHTY8MKBZ4mGSAhJQcTtSPqsqMs"
 };
